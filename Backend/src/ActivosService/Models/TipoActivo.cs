8b72e158c5b50ddfb62966c9e5b4af3d15ed36aa
zeveex <evelyn804marcela@gmail.com>
2026-09-23 07:41:58 -0500

    Ajustar configuracion de microservicios

--- Backend/src/ActivosService/Models/TipoActivo.cs
+++
@@ -1,9 +0,0 @@
-namespace ActivosService.Models
-{
-    public class TipoActivo
-    {
-        public int IdTipo { get; set; }
-        public string NombreTipo { get; set; } = "";
-        public int IdCategoria { get; set; }
-    }
-}
--- Backend/src/AuthService/DTOs/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace UsuariosService.DTOs
-{
-    public class LoginDTO
-    {
-        public string Usuario { get; set; } = "";
-        public string Password { get; set; } = "";
-    }
-}
